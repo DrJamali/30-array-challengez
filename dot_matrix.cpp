02e3a0be00ee4d940fa8293ab0e137c6eb9c3635
DrJamali <usibghat670@gmail.com>
2023-07-06 15:02:07 +0500

    kuch bhi

---
+++ dot_matrix.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows, col;
+    cout << "Enter rows: ";
+    cin >> rows;
+    cout << "Enter Coloumns: ";
+    cin >> col;
+    int mat[rows][col];
+    for (int i = 0; i < rows; i++)
+    {
+        cout << "Enter the " << i + 1 << " row: ";
+        for (int j = 0; j < col; j++)
+        {
+            cin >> mat[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            
+        }
+        
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+    return 0;
+}
